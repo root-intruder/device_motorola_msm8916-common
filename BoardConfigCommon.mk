dae857fce17dd3ed58e057fc1a13f6b4768319d6
Kyle Harrison <khwebmail@gmail.com>
2019-09-12 13:11:26 +0100

    [DNM] msm8916-common: Add temporary sepolicies for 11 bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,6 +152,7 @@ BOARD_ROOT_EXTRA_FOLDERS := firmware persist fsg
 # SELinux
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy_tmp
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
